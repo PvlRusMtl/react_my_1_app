5f47f0649f5f29ee53408f784dc591d14ab8c81d
Rustam <rustam.zholdubayev@gmail.com>
2022-07-21 22:15:39 -0400

    123 elements and components

--- src/index.js
+++ src/index.js
@@ -2,19 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-const text = 'Hello World!';
-
-const elem = (
-  <div>
-    <h2 className='text'>Text: {text}</h2>
-    <input type="text"/>
-    <label htmlFor=""></label>
-    <button tabIndex='0'>Click</button>
-  </div>
-);
+import { Header } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App/>
 );
