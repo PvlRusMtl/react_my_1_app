eb259b4d43034b7529a8a42621f659c18c647006
Rustam <rustam.zholdubayev@gmail.com>
2022-08-01 13:11:46 -0400

    143 styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import { Component
  } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
+
+const EmpItem = styled.div`
+  padding:20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, .2);
+  a {
+    display: block;
+    margin: 10px 0 10px 0;
+    color: ${props => props.active ? 'orange' : 'black'};
+  }
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+`;
+
+const Header = styled.h2`
+  font-size:22px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  padding:5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0, 0, 0, .2);
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, .2);
+`;
 
 class WhoAmI extends Component {
   constructor(props) {
@@ -27,29 +57,35 @@ class WhoAmI extends Component {
   render() {
     const {name, surname, link} = this.props;
     const{position, years} = this.state;
+    console.log(this);
 
     return (
-      <div>
-        <button onClick={this.nextYear}>+++</button>
-        <h1>My name is {name}, surname - {surname},
+      <EmpItem active>
+        <Button onClick={this.nextYear}>+++</Button>
+        <Header>My name is {name}, surname - {surname},
             age - {years},
-            position - {position}</h1>
+            position - {position}</Header>
         <a href={link}>My profile</a>
         <form>
           <span>Tape your position</span>
           <input type='text' onChange={(e) => this.commitInputChanges(e, 'some color')} />
         </form>
-      </div>
+      </EmpItem>
     )
   }
 }
 
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
       <WhoAmI name='John' surname='Smith' link='pikabu.ru'/>
       <WhoAmI name='Alex' surname='Grey' link='youtube.com'/>
-    </div>
+    </Wrapper>
   );
 }
 
