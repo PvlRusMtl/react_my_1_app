f962a73ac9320338449af7fa0242cb5a49d519c9
Rustam <rustam.zholdubayev@gmail.com>
2022-08-11 14:16:17 -0400

    156 props.children

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Button } from './App';
+import {Button} from './App';
 import styled from 'styled-components';
-import BootstrapTest
- from './BootstrapTest';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BigButton = styled(Button)`
-        margin: 0 auto;
-        width: 245px;
-        text-align: center;
+    margin: 0 auto;
+    width: 245px;
+    text-align: center;
 `;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <StrictMode>
-        <App/>
-        <BigButton as='a'>Send message</BigButton>
-        <BootstrapTest/>
-  </StrictMode>  
+      <App/>
+      <BigButton as="a">Отправить отчет</BigButton>
+  </StrictMode>,
+  document.getElementById('root')
 );
