bd7c1b945e52b027bef6e5d8fa2cebc982004e68
Rustam <rustam.zholdubayev@gmail.com>
2022-08-12 13:51:15 -0400

    159 ref

--- src/App.js
+++ src/App.js
@@ -1,163 +1,68 @@
-import React,{ Component } from 'react';
-import styled from 'styled-components';
-import BootstrapTest from './BootstrapTest';
-
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
 
-const EmpItem = styled.div`
-    padding: 20px;
-    margin-bottom: 15px;
-    border-radius: 5px;
-    box-shadow: 5px 5px 10px rgba(0,0,0, .2);
-    a {
-        display: block;
-        margin: 10px 0 10px 0;
-        color: ${props => props.active ? 'orange' : 'black'};
+class Form extends Component {
+        // myRef = React.createRef();
+
+    // componentDidMount() {
+    //     this.myRef.current.focus();
+    // }
+
+    setInputRef = elem => {
+        this.myRef = elem;
     }
-    input {
-        display: block;
-        margin-top: 10px;
-    }
-`;
 
-const Header = styled.h2`
-    font-size: 22px;
-`;
-
-export const Button = styled.button`
-    display: block;
-    padding: 5px 15px;
-    background-color: gold;
-    border: 1px solid rgba(0,0,0, .2);
-    box-shadow: 5px 5px 10px rgba(0,0,0, .2);
-`;
-
-class WhoAmI extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            years: 27,
-            position: ''
+    focusFirstTI = () => {
+        if (this.myRef) {
+        this.myRef.focus();
         }
     }
 
-    nextYear = () => {
-        this.setState(state => ({
-            years: state.years + 1
-        }))
-    }
-
-    commitInputChanges = (e, color) => {
-        console.log(color);
-        this.setState({
-            position: e.target.value
-        })
-    }
-
     render() {
-        const {name, surname, link} = this.props;
-        const {position, years} = this.state;
-
         return (
-            <EmpItem active>
-                <Button onClick={this.nextYear}>+++</Button>
-                <Header>My name is {name}, surname - {surname}, 
-                    age - {years}, 
-                    position - {position}</Header>
-                <a href={link}>My profile</a>
-                <form>
-                    <span>Введите должность</span>
-                    <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+            <Container>
+                <form className="w-50 border mt-5 p-3 m-auto">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input 
+                        ref={this.setInputRef}
+                        type="email" 
+                        className="form-control" 
+                        id="exampleFormControlInput1" 
+                        placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea onClick={this.focusFirstTI} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
                 </form>
-            </EmpItem>
+            </Container>
         )
     }
 }
 
-const Wrapper = styled.div`
-    width: 600px;
-    margin: 80px auto 0 auto;
-`;
+class TextInput extends Component {
 
-const DynamicGreeting = (props) => {
-    return (
-        <div className={'mb-3 p-3 border border-' + props.color}>
-            {
-                React.Children.map(props.children, child => {
-                    return React.cloneElement(child, {className: 'shadow p-3 m-3 border rounded'})
-                })
-            }
-        </div>
-    )
-}
-
-const HelloGreetings = () => {
-    return (
-        <div style={{'width': '600px', 'margin': '0 auto'}}>
-            <DynamicGreeting color={'primary'}>
-                <h2>Hello world</h2>
-            </DynamicGreeting>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <h2>The counter is {props.counter}</h2>
-    )
-}
-
-class Counter extends Component {
-    state = {
-        counter: 0
+    doSmth = () => {
+        console.log('smth');
     }
-
-    changeCounter = () => {
-        this.setState(({counter}) => ({
-            counter: counter + 1
-        }))
-    }
-
-    render() {
+    
+    render () {
         return (
-            <>
-                <button
-                className={'btn btn-primary'}
-                onClick={this.changeCounter}>
-                    Click me
-                </button>
-                {this.props.render(this.state.counter)}
-            </>
+            <input 
+            type="email" 
+            className="form-control" 
+            id="exampleFormControlInput1" 
+            placeholder="name@example.com"/>
         )
     }
 }
 
 function App() {
-  return (
-    <Wrapper>
-
-        <Counter render={counter => (
-            <Message counter={counter}/>
-        )}/>
-
-        <HelloGreetings/>
-        <BootstrapTest
-            left = {
-                <DynamicGreeting color={'primary'}>
-                    <h2>This meet was hard</h2>
-                    <h2>Hello world</h2>
-                </DynamicGreeting>
-            }
-            right = {
-                <DynamicGreeting color={'primary'}>
-                    <h2>whoa</h2>
-                </DynamicGreeting>
-            }
-        />
-        <WhoAmI name='John' surname="Smith" link="facebook.com"/>
-        <WhoAmI name='Alex' surname="Shepard" link="vk.com"/>
-    </Wrapper>
-  );
+    return (
+        <Form/>
+    );
 }
 
 export default App;
